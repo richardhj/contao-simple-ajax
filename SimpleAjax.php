776b5fa224492f9c268e6eb8eda4827a05c1425f
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2016-10-07 19:54:06 +0200

    CS

--- SimpleAjax.php
+++ SimpleAjax.php
@@ -20,23 +20,12 @@ require('system/initialize.php');
 /**
  * Class SimpleAjax
  * Contains methods to catch ajax requests and send responses back to the client.
- *
  */
-class SimpleAjax extends Controller
+class SimpleAjax extends \Controller
 {
 
     /**
-     * Call the parent constructor.
-     *
-     * !!! DON'T REMOVE THIS !!!
-     *
-     * If you remove this you get the following error message:
-     * Fatal error: Call to protected System::__construct() from invalid
-     * context
-     *
-     * @param    void
-     *
-     * @return    void
+     * Call the parent constructor
      */
     public function __construct()
     {
@@ -46,16 +35,12 @@ class SimpleAjax extends Controller
 
     /**
      * Get the ajax request and call all hooks
-     *
-     * @param    void
-     *
-     * @return    void
      */
     public function run()
     {
-        // check if a hook is registered
+        // Check if a hook is registered
         if (is_array($GLOBALS['TL_HOOKS']['simpleAjax']) && count($GLOBALS['TL_HOOKS']['simpleAjax']) > 0) {
-            // execute every registered callback
+            // Execute every registered callback
             foreach ($GLOBALS['TL_HOOKS']['simpleAjax'] as $callback) {
                 if (is_array($callback)) {
                     System::importStatic($callback[0])->{$callback[1]};
@@ -65,13 +50,12 @@ class SimpleAjax extends Controller
             }
         }
 
-        // if there is no other output, we generate a 412 error response
+        // If there is no other output, we generate a 412 error response
         header('HTTP/1.1 412 Precondition Failed');
         die('Simple Ajax: Invalid AJAX call.');
     }
 }
 
-
 // create a SimpleAjax instance and run it
-$objSimpleAjax = new SimpleAjax();
-$objSimpleAjax->run();
+$simpleAjax = new SimpleAjax();
+$simpleAjax->run();
--- SimpleAjaxFrontend.php
+++ SimpleAjaxFrontend.php
@@ -20,44 +20,36 @@ require('system/initialize.php');
  * Class SimpleAjaxFrontend
  * Contains methods to catch ajax requests and send responses back to the client.
  */
-class SimpleAjaxFrontend extends Frontend
+class SimpleAjaxFrontend extends \Frontend
 {
 
     /**
      * Set some important constants
-     *
-     * @param    void
-     *
-     * @return    void
      */
     public function __construct()
     {
         FrontendUser::getInstance();
 
-        // call the constructor from Frontend
+        // Call the constructor from Frontend
         parent::__construct();
 
-        // check if a user is logged in
+        // Check if a user is logged in
         define('BE_USER_LOGGED_IN', $this->getLoginStatus('BE_USER_AUTH'));
         define('FE_USER_LOGGED_IN', $this->getLoginStatus('FE_USER_AUTH'));
 
-        // set static url's in case the user generated HTML code
+        // Set static url's in case the user generated HTML code
         \Controller::setStaticUrls();
     }
 
 
     /**
      * Get the ajax request and call all hooks
-     *
-     * @param    void
-     *
-     * @return    void
      */
     public function run()
     {
-        // run the global hook
+        // Run the global hooks
         if (is_array($GLOBALS['TL_HOOKS']['simpleAjax']) && count($GLOBALS['TL_HOOKS']['simpleAjax']) > 0) {
-            // execute every registered callback
+            // Execute every registered callback
             foreach ($GLOBALS['TL_HOOKS']['simpleAjax'] as $callback) {
                 if (is_array($callback)) {
                     System::importStatic($callback[0])->{$callback[1]};
@@ -67,12 +59,11 @@ class SimpleAjaxFrontend extends Frontend
             }
         }
 
-        // run the frontend exclusive hook
-        if (is_array($GLOBALS['TL_HOOKS']['simpleAjaxFrontend']) && count(
-                $GLOBALS['TL_HOOKS']['simpleAjaxFrontend']
-            ) > 0
+        // Run the frontend exclusive hooks
+        if (is_array($GLOBALS['TL_HOOKS']['simpleAjaxFrontend'])
+            && count($GLOBALS['TL_HOOKS']['simpleAjaxFrontend']) > 0
         ) {
-            // execute every registered callback
+            // Execute every registered callback
             foreach ($GLOBALS['TL_HOOKS']['simpleAjaxFrontend'] as $callback) {
                 if (is_array($callback)) {
                     System::importStatic($callback[0])->{$callback[1]};
@@ -82,13 +73,12 @@ class SimpleAjaxFrontend extends Frontend
             }
         }
 
-        // if there is no other output, we generate a 412 error response
+        // If there is no other output, we generate a 412 error response
         header('HTTP/1.1 412 Precondition Failed');
         die('Simple Ajax: Invalid AJAX call.');
     }
 }
 
-
 // create a SimpleAjax instance and run it
-$objSimpleAjaxFrontend = new SimpleAjaxFrontend();
-$objSimpleAjaxFrontend->run();
+$simpleAjaxFrontend = new SimpleAjaxFrontend();
+$simpleAjaxFrontend->run();
